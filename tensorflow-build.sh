d966c3168a8616e8d04095f01b1d3e187a6a7b94
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2020-02-25 17:14:50 +0100

    docker update

--- tensorflow-build.sh
+++ tensorflow-build.sh
@@ -5,5 +5,6 @@ set -x
 cd /usr/src/daloflow/tensorflow
 ./configure
 bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package --action_env PYTHON_BIN_PATH=/usr/bin/python3 
-pip3 install /usr/src/tensorflow/tensorflow-*.whl
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+pip3 install /tmp/tensorflow_pkg/tensorflow-*.whl
 
