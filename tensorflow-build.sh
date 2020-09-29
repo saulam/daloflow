f80949d697f68471558dc3ea909890e253f4ee36
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2020-09-29 20:29:38 +0200

    1.0

--- tensorflow-build.sh
+++ tensorflow-build.sh
@@ -5,6 +5,7 @@ cd /usr/src/daloflow/tensorflow
 
 yes "" | $(which python3) configure.py
 bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package --action_env PYTHON_BIN_PATH=/usr/bin/python3 
+# bazel build --config=opt  //tensorflow:libtensorflow.so --action_env PYTHON_BIN_PATH=/usr/bin/python3
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /usr/src/daloflow/tensorflow/tensorflow_pkg
 pip3 install /usr/src/daloflow/tensorflow/tensorflow_pkg/tensorflow-*.whl
 
