babbf986227b943774af68bc3982475059563b01
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2020-10-14 22:50:19 +0200

    dalaflow.sh updated

--- daloflow.sh
+++ daloflow.sh
@@ -2,6 +2,26 @@
 #set -x
 
 
+#*
+#*  Copyright 2019-2020 Saul Alonso Monsalve, Felix Garcia Carballeira, Jose Rivadeneira Lopez-Bravo, Alejandro Calderon Mateos,
+#*
+#*  This file is part of DaLoFlow.
+#*
+#*  DaLoFlow is free software: you can redistribute it and/or modify
+#*  it under the terms of the GNU Lesser General Public License as published by
+#*  the Free Software Foundation, either version 3 of the License, or
+#*  (at your option) any later version.
+#*
+#*  WepSIM is distributed in the hope that it will be useful,
+#*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#*  GNU Lesser General Public License for more details.
+#*
+#*  You should have received a copy of the GNU Lesser General Public License
+#*  along with WepSIM.  If not, see <http://www.gnu.org/licenses/>.
+#*
+
+
 daloflow_help ()
 {
 	echo ""
@@ -283,6 +303,7 @@ do
 		A=$1
 		CNAME=$(docker ps -f name=daloflow -q | head -1)
 		docker container exec -it $CNAME mpirun -np $NP -machinefile machines_mpi -bind-to none -map-by slot  --allow-run-as-root $A
+	      # docker container exec -it $CNAME horovodrun -np $NP -hostfile machines_horovod $A
 	     ;;
 	     bash)
 		shift
