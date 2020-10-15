0d76fa411675d7cf0000a0101f22224b5d84a69c
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2020-10-15 21:32:58 +0200

    dalaflow.sh updated

--- mk_datasets.sh
+++ mk_datasets.sh
@@ -2,13 +2,12 @@
 #set -x
 
 SIZES="32 64 128 256 512 1024"
-NIMG="1000000"
+N_IMG_TRAIN="1000000"
+N_IMG_TEST="1000"
 
 
 for S in $SIZES; do
-for NI in $NIMG; do
 	echo " * "$NI images of $S"x"$S"..."
-	echo python3 horovod/examples/dump_dataset.py --height $S --width $S --ntrain $NIMG --ntest 1000
-done
+	echo python3 horovod/examples/dump_dataset.py --height $S --width $S --ntrain $N_IMG_TRAIN --ntest $N_IMG_TEST
 done
 
