ca8c192b48dc05d05dac447651bfe180919a5a05
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2020-11-08 20:43:49 +0100

    daloflow.sh updated

--- daloflow.sh
+++ daloflow.sh
@@ -22,12 +22,16 @@
 #*
 
 
-daloflow_help ()
+daloflow_welcome ()
 {
 	echo ""
-	echo "  daloflow 3.2"
+	echo "  daloflow 3.5"
 	echo " --------------"
 	echo ""
+}
+
+daloflow_help ()
+{
 	echo ": For first time deployment, please execute:"
 	echo "  $0 postclone"
 	echo "  $0 prerequisites"
@@ -35,13 +39,13 @@ daloflow_help ()
 	echo ""
 	echo ": For a typical single node work session (with 4 containers and 2 process), please execute:"
 	echo "  $0 start 4"
-	echo "  $0 mpirun 2 \"python3.7 ./horovod/examples/tensorflow2_keras_mnist_saul.py --height 32 --width 32 --path dataset32x32\""
+	echo "  $0 mpirun 2 \"python3 ./horovod/examples/tensorflow2_keras_mnist_saul.py --height 32 --width 32 --path dataset32x32\""
 	echo "  ..."
 	echo "  $0 stop"
 	echo ""
 	echo ": For a typical multinode work session (with 4 containers and 2 process), please execute:"
 	echo "  $0 swarm-start 4"
-	echo "  $0 mpirun 2 \"python3.7 ./horovod/examples/tensorflow2_keras_mnist_saul.py --height 32 --width 32 --path dataset32x32\""
+	echo "  $0 mpirun 2 \"python3 ./horovod/examples/tensorflow2_keras_mnist_saul.py --height 32 --width 32 --path dataset32x32\""
 	echo "  ..."
 	echo "  $0 swarm-stop"
 	echo ""
@@ -50,7 +54,19 @@ daloflow_help ()
 	echo "  $0 docker_in_docker"
 	echo "  $0 save | load | status | test"
 	echo ""
-	echo ": Please read the README.md file for more information."
+	echo ": Please read the README.md file for more information (github.com/saulam/daloflow/)."
+	echo ""
+}
+
+daloflow_info_no_start ()
+{
+	echo ": Please start/swarm-start first:"
+	echo ""
+        echo ": * For a typical single node work session (with 4 containers and 2 process), please execute:"
+        echo "    ./daloflow.sh start       <number of containers>"
+	echo ""
+        echo ": * For a typical multinode work session (with 4 containers and 2 process), please execute:"
+        echo "    ./daloflow.sh swarm-start <number of containers>"
 	echo ""
 }
 
@@ -115,11 +131,14 @@ daloflow_prerequisites ()
 # Image
 #
 
-daloflow_build_image ()
+daloflow_build_base_image ()
 {
 	echo "Building initial image..."
 	docker image build -t daloflow:v2 .
+}
 
+daloflow_build_derivated_image ()
+{
 	echo "Building compilation image..."
 	daloflow_start 1
         daloflow_build_all
@@ -199,15 +218,25 @@ daloflow_start ()
 	echo "wating $NC seconds..."
 	sleep $NC
 
-	# Setup container cluster (in single node)
+	# Container cluster (in single node) files...
 	CONTAINER_ID_LIST=$(docker ps -f name=daloflow -q)
 	docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID_LIST > machines_mpi
-	docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID_LIST | sed 's/.*/& slots=1/g' > machines_horovod
+	docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID_LIST | sed 's/.*/& slots=1 max_slots=16/g' > machines_horovod
 
 	if [ $(getent group daloflow) ]; then
 	     chgrp daloflow machines_*
 	fi
 
+}
+
+daloflow_stop ()
+{
+	# stop composition
+	docker-compose -f Dockercompose.yml down
+
+        # Remove container cluster files...
+	rm -fr machines_mpi
+	rm -fr machines_horovod
 }
 
 daloflow_swarm_start ()
@@ -222,11 +251,66 @@ daloflow_swarm_start ()
         docker stack deploy --compose-file Dockerstack.yml daloflow
         docker service scale daloflow_node=$NC
 
-        # Setup container cluster
+        # Container cluster files...
         CONTAINER_ID_LIST=$(docker service ps daloflow_node -f desired-state=running -q)
         docker inspect -f '{{range .NetworksAttachments}}{{.Addresses}}{{end}}' $CONTAINER_ID_LIST | sed "s/^\[//g" | awk 'BEGIN {FS="/"} ; {print $1}' > machines_mpi
         cat machines_mpi | sed 's/.*/& slots=1/g' > machines_horovod
 }
+
+daloflow_swarm_stop ()
+{
+	# Stop service
+	docker service rm daloflow_node
+
+        # Remove container cluster files...
+	rm -fr machines_mpi
+	rm -fr machines_horovod
+}
+
+daloflow_run ()
+{
+	# Get parameters
+	NP=$1
+	shift
+	A=$@
+	CNAME=$(docker ps -f name=daloflow -q | head -1)
+
+	# Check parameters
+	if [ "x$CNAME" == "x" ]; then
+            daloflow_welcome
+            daloflow_info_no_start
+            exit
+	fi
+
+        # daloflow:v2 in TUCAN
+	docker container exec -it $CNAME     \
+	       mpirun -np $NP -machinefile machines_horovod \
+		      -bind-to none -map-by slot -verbose --allow-run-as-root \
+		       -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
+		       -x NCCL_SOCKET_IFNAME=^lo,docker0 \
+		       -mca pml ob1 -mca btl ^openib \
+		       -mca btl_tcp_if_exclude lo,docker0,eth1 \
+		      $A
+}
+
+daloflow_bash_node ()
+{
+	# Check parameters
+	if [ ! -f machines_mpi ]; then
+            daloflow_welcome
+            daloflow_info_no_start
+            exit
+	fi
+
+	CIP=$(head -$1 machines_mpi | tail -1)
+	CNAME=$(docker ps -f name=daloflow -q | head -1)
+	docker container exec -it $CNAME /usr/bin/ssh $CIP
+}
+
+
+#
+# Testing
+#
 
 daloflow_test ()
 {
@@ -257,6 +341,7 @@ daloflow_test_node ()
 
 # Usage
 if [ $# -eq 0 ]; then
+        daloflow_welcome
 	daloflow_help $0
 	exit
 fi
@@ -275,8 +360,7 @@ do
 
 	     # image
 	     build|image)
-	        echo "Building initial image..."
-	        docker image build -t daloflow:v2 .
+                daloflow_build_base_image
 	     ;;
 	     save)
                 daloflow_save
@@ -285,7 +369,8 @@ do
                 daloflow_load
 	     ;;
 	     build_image)
-		daloflow_build_image
+                daloflow_build_base_image
+		daloflow_build_derivated_image
 	     ;;
 	     build_all)
 		daloflow_build_all
@@ -297,52 +382,29 @@ do
 	     # single node
 	     start)
 		shift
-		daloflow_start $1
+		daloflow_start $@
 	     ;;
 	     stop)
-		docker-compose -f Dockercompose.yml down
-	        rm -fr machines_mpi
-	        rm -fr machines_horovod
+                daloflow_stop
 	     ;;
 
 	     # multinode node
 	     swarm-start)
 		shift
-		daloflow_swarm_start $1
+		daloflow_swarm_start $@
 	     ;;
 	     swarm-stop)
-                docker service rm daloflow_node
-	        rm -fr machines_mpi
-	        rm -fr machines_horovod
+                daloflow_swarm_stop
 	     ;;
 
 	     # mpirun + bash
 	     mpirun|swarm-mpirun)
 		shift
-		NP=$1
-		shift
-		A=$1
-		CNAME=$(docker ps -f name=daloflow -q | head -1)
-
-	      # docker container exec -it $CNAME mpirun -np $NP -machinefile machines_mpi -bind-to none -map-by slot  --allow-run-as-root $A
-	      # docker container exec -it $CNAME mpirun -np $NP -machinefile machines_mpi -bind-to none -map-by slot                      $A
-	      # docker container exec -it $CNAME horovodrun -np $NP -hostfile machines_horovod $A
-
-              # daloflow:v2 in TUCAN working !!!
-                docker container exec -it $CNAME     \
-                       mpirun -np $NP -machinefile machines_horovod \
-                              -bind-to none -map-by slot -verbose --allow-run-as-root \
-                               -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
-                               -x NCCL_SOCKET_IFNAME=^lo,docker0 \
-                               -mca pml ob1 -mca btl ^openib \
-                               -mca btl_tcp_if_exclude lo,docker0,eth1 \
-                              $A
+                daloflow_run $@
 	     ;;
 	     bash)
 		shift
-		CIP=$(head -$1 machines_mpi | tail -1)
-		CNAME=$(docker ps -f name=daloflow -q | head -1)
-		docker container exec -it $CNAME /usr/bin/ssh $CIP
+                daloflow_bash_node $@
 	     ;;
 
 	     # single node utilities
@@ -361,6 +423,7 @@ do
 
 	     # help
 	     *)
+                daloflow_welcome
 		daloflow_help $0
 	     ;;
 	esac
